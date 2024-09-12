913307326a007d18630726bbd0f7998a6240ffa3
magahon-f <ma.trufakina@gmail.com>
2024-09-12 12:45:12 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    Error() { }
+
+    Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Is active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error errorDefault = new Error();
+        errorDefault.printInfo();
+        Error errorFalse = new Error(false, 1, "Good work.");
+        errorFalse.printInfo();
+        Error errorTrue = new Error(true, 2, "Okay, Houston ... we've had a problem here.");
+        errorTrue.printInfo();
+    }
+
+}
